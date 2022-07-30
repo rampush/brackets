98f0709f588426cf265252185b4235b3cc1d1555
Arsenii Ogarkov <ogarkovar@yandex.ru>
2022-07-30 19:44:54 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arrstr = str.split('');
+  for (let i = 0; i < arrstr.length; i++) {
+    for (let k = 0; k < bracketsConfig.length; k++) {
+      if (arrstr[i] == bracketsConfig[k][1] && arrstr[i - 1] == bracketsConfig[k][0]) {
+        arrstr.splice(i - 1, 2);
+        i = 0;
+        break;
+      }
+    }
+  }
+  if (arrstr < 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
